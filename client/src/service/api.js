dfc9a45728bc2267be379d614239db31f318c7ab
007Ayushi <411ayushixisc@gmail.com>
2026-04-30 23:07:34 +0530

    Updating frontend

--- client/src/service/api.js
+++ client/src/service/api.js
@@ -1,41 +1,36 @@
-import axios from 'axios';
+import axios from "axios";
 
+const api = axios.create({
+  baseURL:
+    process.env.REACT_APP_API_URL || "https://live-class-project.onrender.com/api",
+  headers: { "Content-Type": "application/json" },
+});
 
-//create a axios instance with base configuration 
-const api = axios.create(
-    {
-        baseURL: process.env.REACT_APP_API_URL,
-         headers: { 'Content-Type': 'application/json' }
-    }
-)
-
+console.log("API BASE URL:", api.defaults.baseURL);
 
 api.interceptors.request.use(
-    (config) =>{
-        const token = localStorage.getItem('token');
-        if(token){
-            config.headers.Authorization = `Bearer ${token}`
-        }
-        return config;
-    },
-    (error) => {
-        return Promise.reject(error);
+  (config) => {
+    const token = localStorage.getItem("token");
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
     }
-)
 
-
-//Response interceptor - hanlde error globally
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
 
 api.interceptors.response.use(
-    (response) =>  response,
-    (error) => {
-        if(error.response?.status === 401){
-            localStorage.removeItem('token')
-            window.location.href ='/login'
-        }
-    return Promise.reject(error);
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      localStorage.removeItem("token");
+      window.location.href = "/login";
     }
-)
 
+    return Promise.reject(error);
+  }
+);
 
 export default api;
